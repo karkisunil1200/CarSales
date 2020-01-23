e4eaac95b42ffcca3012cb2a763505e32aa7e3f8
Sunil Karki <karkisunil1200@gmail.com>
2020-01-23 10:24:29 -0600

    Connected store to the app component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {addReducer} from './reducers/addReducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+const store = createStore(addReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
